531594738b2e2098fac195b24c70f4937e0bf383
BureikuT <bureikut@hotmail.com>
2020-06-03 12:09:08 -0600

    created LinkedList.ts and changed Sorter into an abstract class

--- build/index.js
+++ build/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_1 = require("./Sorter");
-var CharactersCollection_1 = require("./CharactersCollection");
 // const numbersCollection = new NumbersCollection([10, 3, -5, 0])
-// const sorter = new Sorter(numbersCollection);
-//   sorter.sort();
+// numbersCollection.sort()
 //   console.log(numbersCollection.data);
-var charactersCollection = new CharactersCollection_1.CharactersCollection("Xaayb");
-var sorter = new Sorter_1.Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection("Xaayb");
+// charactersCollection.sort()
+// console.log(charactersCollection.data)
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+// linkedList.sort()
+// linkedList.print();
